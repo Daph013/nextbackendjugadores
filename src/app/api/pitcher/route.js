39696aab922766d5d45400913790a9afa863a191
david <daph013@gmail.com>
2024-09-14 14:22:50 -0400

    first commit

---
+++ src/app/api/pitcher/route.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import pool from "../../libs/mysql";
+
+export async function GET() {
+    try {
+        const db = await pool.getConnection()
+        const query = 'select * from pitchers'
+        const [rows] = await db.execute(query)
+        db.release()
+        
+       // return NextResponse.json(rows)
+       return new Response(JSON.stringify(rows), {
+        // Set CORS headers here
+        headers: {
+          'Access-Control-Allow-Origin': '*', // Allow requests from all origins (replace with specific origins if needed)
+          'Access-Control-Allow-Methods': 'GET', // Allowed methods (adjust as needed)
+          'Access-Control-Allow-Headers': 'Content-Type', // Allowed headers (adjust as needed)
+        },
+      });
+    } catch (error) {
+        return NextResponse.json({
+            error: error
+        }, { status: 500 })
+    }
+}
